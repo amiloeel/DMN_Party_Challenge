1052449b47fa54641950a75a25a273543e50b622
pamAdmin <>
2021-12-17 09:51:13 -0300

    {/src/main/java/com/newspace/festinha/Evento.java}

--- src/main/java/com/newspace/festinha/Evento.java
+++ src/main/java/com/newspace/festinha/Evento.java
@@ -6,12 +6,35 @@ package com.newspace.festinha;
 
 public class Evento implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Evento() {
-    }
+	@org.kie.api.definition.type.Label(value = "Nome do Evento")
+	private java.lang.String titulo;
+	@org.kie.api.definition.type.Label(value = "Data do Evento")
+	private java.util.Date data;
 
+	public Evento() {
+	}
 
+	public java.lang.String getTitulo() {
+		return this.titulo;
+	}
 
+	public void setTitulo(java.lang.String titulo) {
+		this.titulo = titulo;
+	}
+
+	public java.util.Date getData() {
+		return this.data;
+	}
+
+	public void setData(java.util.Date data) {
+		this.data = data;
+	}
+
+	public Evento(java.lang.String titulo, java.util.Date data) {
+		this.titulo = titulo;
+		this.data = data;
+	}
 
 }
